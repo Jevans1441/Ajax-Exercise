f7e676f98b036bbea4ec4d9664ae24ac204ffb82
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-11-13 13:33:02 -0500

    added temps & css

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
+weather = document.querySelector("#weather");
+city = document.querySelector("#city");
+feelsLike = document.querySelector("#feelsLike");
+lows = document.querySelector("#lows");
+highs = document.querySelector("#highs");
+
 fetch(
   "http://api.openweathermap.org/data/2.5/weather?q=Athens,US-GA&appid=710540d456e691680abdfc45926ba1b8&units=imperial"
 )
   .then((Response) => Response.json())
   .then((body) => {
-    document.getElementById("weather").innerHTML = body.main.temp + "°";
-  })
-  .catch((error) => alert("Please add your api key to the fetch url"));
+    city.innerHTML = body.name;
+    weather.innerHTML = body.main.temp + "°";
+    feelsLike.innerHTML = body.main.feels_like;
+    lows.innerHTML = body.main.temp_min;
+    highs.innerHTML = body.main.temp_max;
+    console.log(body);
+  });
+//   .catch((error) => alert("Please add your api key to the fetch url"));
